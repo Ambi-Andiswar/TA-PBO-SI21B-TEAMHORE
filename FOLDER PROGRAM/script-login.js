78b1c3cc7bc37be24a6c57f2781209939170e335
bayuanggoro31 <bayundagel123@gmail.com>
2024-01-19 22:45:21 +0700

    bayu: mengedit page login

--- FOLDER PROGRAM/script-login.js
+++ FOLDER PROGRAM/script-login.js
@@ -1,17 +1,48 @@
-function validateForm() {
-    var username = document.getElementById('username').value;
-    var password = document.getElementById('password').value;
-    var popup = document.getElementById('popup');
-    var popuptext = document.getElementById('popuptext');
+const form = document.querySelector("form");
+eField = form.querySelector(".email"),
+    eInput = eField.querySelector("input"),
+    pField = form.querySelector(".password"),
+    pInput = pField.querySelector("input");
 
-    if (username === '' || password === '') {
-        popuptext.textContent = 'Harap isi semua kolom';
-        popup.style.display = 'block';
-        setTimeout(function () {
-            popup.style.display = 'none';
-        }, 3000); // Menyembunyikan popup setelah 3 detik
-    } else {
-        popup.style.display = 'none';
-        // Implementasi logika autentikasi atau pengiriman data ke server dapat ditambahkan di sini.
+form.onsubmit = (e) => {
+    e.preventDefault();
+
+    (eInput.value == "") ? eField.classList.add("shake", "error"): checkEmail();
+    (pInput.value == "") ? pField.classList.add("shake", "error"): checkPass();
+
+    setTimeout(() => {
+        eField.classList.remove("shake");
+        pField.classList.remove("shake");
+    }, 500);
+
+    eInput.onkeyup = () => { checkEmail(); }
+    pInput.onkeyup = () => { checkPass(); }
+
+    function checkEmail() {
+        let pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+        if (!eInput.value.match(pattern)) {
+            eField.classList.add("error");
+            eField.classList.remove("valid");
+            let errorTxt = eField.querySelector(".error-txt");
+
+            (eInput.value != "") ? errorTxt.innerText = "Enter a valid email address": errorTxt.innerText = "Email can't be blank";
+        } else {
+            eField.classList.remove("error");
+            eField.classList.add("valid");
+        }
+    }
+
+    function checkPass() {
+        if (pInput.value == "") {
+            pField.classList.add("error");
+            pField.classList.remove("valid");
+        } else {
+            pField.classList.remove("error");
+            pField.classList.add("valid");
+        }
+    }
+
+    if (!eField.classList.contains("error") && !pField.classList.contains("error")) {
+        window.location.href = form.getAttribute("action");
     }
 }
